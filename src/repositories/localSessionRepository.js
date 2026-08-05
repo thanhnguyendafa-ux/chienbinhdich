3656d77814898f6c2d28904d2838b1300bccdda5
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-05 08:55:19 +0700

    Advance persisted sessions to mastery schema v5

--- src/repositories/localSessionRepository.js
+++ src/repositories/localSessionRepository.js
@@ -1,8 +1,8 @@
 import { SESSION_SCHEMA_VERSION } from '../core/sessionMachine.js';
 
-const ACTIVE_KEY = 'cbd.activeSession.v4';
+const ACTIVE_KEY = 'cbd.activeSession.v5';
 const LAST_NAME_KEY = 'cbd.lastStudentName.v1';
-const REPORT_PREFIX = 'cbd.report.v4.';
+const REPORT_PREFIX = 'cbd.report.v5.';
 
 export const localSessionRepository = Object.freeze({
   saveActive(session) {
