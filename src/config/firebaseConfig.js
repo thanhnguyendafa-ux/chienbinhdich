aad548a8aebddf9273d55a19853284b13b11a755
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-09 00:59:31 +0700

    Configure correct Chiến Binh Dịch Firebase project
    Populate the verified public Firebase Web App config for project chienbinhdich while keeping Firebase writes disabled pending Anonymous Auth and Firestore Rules.

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,9 +1,11 @@
 export const firebaseConfig = Object.freeze({
   enabled: false,
   project: Object.freeze({
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    appId: ''
+    apiKey: 'AIzaSyBwME3mRo7UERt110f-uAFUfH6NdjpxLjg',
+    authDomain: 'chienbinhdich.firebaseapp.com',
+    projectId: 'chienbinhdich',
+    storageBucket: 'chienbinhdich.firebasestorage.app',
+    messagingSenderId: '907387062033',
+    appId: '1:907387062033:web:1fca41809802f89fa96eb0'
   })
 });
