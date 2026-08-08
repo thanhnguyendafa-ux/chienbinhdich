efefabe493826aa7eab4b928f2473c92a47f36c0
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-09 00:49:03 +0700

    Remove incorrect Firebase project config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = Object.freeze({
   enabled: false,
   project: Object.freeze({
-    apiKey: 'AIzaSyBsb2aKAneEDhVQksPujhTZZRjw1aXn0VI',
-    authDomain: 'mrt-student-reports.firebaseapp.com',
-    projectId: 'mrt-student-reports',
-    appId: '1:807170856256:web:aa180675617416205196a5'
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    appId: ''
   })
 });
