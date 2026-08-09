cb333ab2f23471f3abfecee67ac890d1d87c5170
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-09 23:31:03 +0700

    Cover Global 5 Review folder in catalog tests

--- tests/catalog.test.js
+++ tests/catalog.test.js
@@ -8,13 +8,14 @@ import { getSetDescriptor, getSetDescriptorBySlug, listFolders, listSetDescripto
 test('catalog exposes hierarchical Global 5, Global 7 and MRT folders with their published Sets', () => {
   assert.deepEqual(validateCatalog(lessonFolders, lessonRegistry), []);
   assert.deepEqual(listFolders().map(folder => folder.id), [
-    'samples', 'global5', 'global5-unit1', 'global5-unit2', 'global7', 'global7-unit1', 'mrt-lessons'
+    'samples', 'global5', 'global5-unit1', 'global5-unit2', 'global5-review', 'global7', 'global7-unit1', 'mrt-lessons'
   ]);
   assert.deepEqual(listSetsByFolder('samples').map(set => set.id), ['g7-u1-mixed-demo', 'g7-u1-s1']);
   assert.deepEqual(listSetsByFolder('global5-unit1').map(set => set.id), [
     'g5-u1-vocab-01', 'g5-u1-pattern-01', 'g5-u1-reading-01', 'g5-u1-writing-01'
   ]);
   assert.deepEqual(listSetsByFolder('global5-unit2').map(set => set.id), ['g5-u2-stress-vocab-01']);
+  assert.deepEqual(listSetsByFolder('global5-review').map(set => set.id), ['g5-review-main-idea-01']);
   assert.deepEqual(listSetsByFolder('global7-unit1').map(set => set.id), ['g7-u1-translation-01', 'g7-u1-translation-02']);
   assert.deepEqual(listSetsByFolder('mrt-lessons').map(set => set.id), ['mrt-g6-gan-aura-action-01', 'mrt-left-cut-right-01']);
 });
