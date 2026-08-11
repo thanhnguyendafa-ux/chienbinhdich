f9166be89db0d2a3a39d30e104743905f573deb0
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-11 17:30:41 +0700

    Aggregate G7 review lesson specs

---
+++ src/data/g7-review-u1-3-specs.js
@@ -0,0 +1,11 @@
+import { g7ReviewU13SpecsPart01 } from './g7-review-u1-3-specs-part01.js';
+import { g7ReviewU13SpecsPart02 } from './g7-review-u1-3-specs-part02.js';
+import { g7ReviewU13SpecsPart03 } from './g7-review-u1-3-specs-part03.js';
+import { g7ReviewU13SpecsPart04 } from './g7-review-u1-3-specs-part04.js';
+
+export const G7_REVIEW_U13_LESSON_SPECS = Object.freeze({
+  ...g7ReviewU13SpecsPart01,
+  ...g7ReviewU13SpecsPart02,
+  ...g7ReviewU13SpecsPart03,
+  ...g7ReviewU13SpecsPart04
+});
