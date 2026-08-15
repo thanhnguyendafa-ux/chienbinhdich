8a3cee7151be303ed899371dd44326fde5a71139
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-15 12:31:02 +0700

    Show student theory before printable true false

--- src/features/admin/print/questions/renderPrintTrueFalse.js
+++ src/features/admin/print/questions/renderPrintTrueFalse.js
@@ -1,7 +1,8 @@
-import { esc, renderTeacherAnswer } from '../printMarkup.js';
+import { esc, renderStudentTheory, renderTeacherAnswer } from '../printMarkup.js';
 
 export function renderPrintTrueFalse(question) {
   return `<article class="lesson-print-question lesson-print-tf" data-print-question="${question.number}">
+    ${renderStudentTheory(question.studentTheory)}
     <div class="lesson-print-inline-response"><p class="lesson-print-prompt"><strong>${question.number}.</strong> ${esc(question.prompt)}</p><span class="lesson-print-blank">________</span></div>
     ${renderTeacherAnswer(question.teacher)}
   </article>`;
