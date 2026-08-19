c98060f5b45c29342230c5add6bb798046daca22
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-19 15:25:04 +0700

    Publish G5 U4 and U5 typing catalogs

--- src/data/publishedLessonCatalog.js
+++ src/data/publishedLessonCatalog.js
@@ -3,6 +3,8 @@ import { gs23WritingFolders, gs23WritingRegistry } from './gs23-writing-typing-c
 import { g5U1WritingFolders, g5U1WritingRegistry } from './g5-u1-writing-typing-published.js';
 import { g5U2WritingFolders, g5U2WritingRegistry } from './g5-u2-writing-typing-published.js';
 import { g5U3WritingFolders, g5U3WritingRegistry } from './g5-u3-writing-typing-published.js';
+import { g5U4WritingFolders, g5U4WritingRegistry } from './g5-u4-writing-typing-published.js';
+import { g5U5WritingFolders, g5U5WritingRegistry } from './g5-u5-writing-typing-published.js';
 import { g5ReviewU15Folders, g5ReviewU15Registry } from './g5-review-u1-5-catalog.js';
 import { g6U1WritingFolders, g6U1WritingRegistry } from './g6-u1-writing-typing-published.js';
 import { g6U2WritingFolders, g6U2WritingRegistry } from './g6-u2-writing-typing-catalog.js';
@@ -15,37 +17,8 @@ import { g7U2WritingFolders, g7U2WritingRegistry } from './g7-u2-writing-typing-
 import { g7ReviewU13Folders, g7ReviewU13Registry } from './g7-review-u1-3-catalog.js';
 
 export const lessonFolders = Object.freeze([
-  ...baseLessonFolders,
-  ...gs23WritingFolders,
-  ...g5U1WritingFolders,
-  ...g5U2WritingFolders,
-  ...g5U3WritingFolders,
-  ...g5ReviewU15Folders,
-  ...g6U1WritingFolders,
-  ...g6U2WritingFolders,
-  ...g6ReviewU13Folders,
-  ...g7U1MlhVocabContextFolders,
-  ...g6U1MlhReadingGapFolders,
-  ...g6U1MlhWritingFolders,
-  ...g7U1WritingFolders,
-  ...g7U2WritingFolders,
-  ...g7ReviewU13Folders
+  ...baseLessonFolders,...gs23WritingFolders,...g5U1WritingFolders,...g5U2WritingFolders,...g5U3WritingFolders,...g5U4WritingFolders,...g5U5WritingFolders,...g5ReviewU15Folders,...g6U1WritingFolders,...g6U2WritingFolders,...g6ReviewU13Folders,...g7U1MlhVocabContextFolders,...g6U1MlhReadingGapFolders,...g6U1MlhWritingFolders,...g7U1WritingFolders,...g7U2WritingFolders,...g7ReviewU13Folders
 ]);
-
 export const lessonRegistry = Object.freeze([
-  ...baseLessonRegistry,
-  ...gs23WritingRegistry,
-  ...g5U1WritingRegistry,
-  ...g5U2WritingRegistry,
-  ...g5U3WritingRegistry,
-  ...g5ReviewU15Registry,
-  ...g6U1WritingRegistry,
-  ...g6U2WritingRegistry,
-  ...g6ReviewU13Registry,
-  ...g7U1MlhVocabContextRegistry,
-  ...g6U1MlhReadingGapRegistry,
-  ...g6U1MlhWritingRegistry,
-  ...g7U1WritingRegistry,
-  ...g7U2WritingRegistry,
-  ...g7ReviewU13Registry
+  ...baseLessonRegistry,...gs23WritingRegistry,...g5U1WritingRegistry,...g5U2WritingRegistry,...g5U3WritingRegistry,...g5U4WritingRegistry,...g5U5WritingRegistry,...g5ReviewU15Registry,...g6U1WritingRegistry,...g6U2WritingRegistry,...g6ReviewU13Registry,...g7U1MlhVocabContextRegistry,...g6U1MlhReadingGapRegistry,...g6U1MlhWritingRegistry,...g7U1WritingRegistry,...g7U2WritingRegistry,...g7ReviewU13Registry
 ]);
