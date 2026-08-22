b62495c25f114f954e0b2c7c5c714498212b1ae9
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-22 08:48:22 +0700

    feat(g6-u2): apply grade 4 trap presentation

--- src/data/g6-u2-trap-content.js
+++ src/data/g6-u2-trap-content.js
@@ -1,11 +1,15 @@
 import { g6U2TrapSourceByKey } from './g6-u2-trap-source.js';
 import { buildG6U2TrapLesson } from './g6-u2-trap-builder.js';
+import { presentG6U2TrapForGrade4 } from './g6-u2-trap-grade4-presentation.js';
 
 const cache = new Map();
 
 export function getG6U2TrapContent(key) {
   const source = g6U2TrapSourceByKey[key];
   if (!source) throw new Error(`Unknown G6 U2 Trap lesson: ${key}`);
-  if (!cache.has(key)) cache.set(key, buildG6U2TrapLesson(source));
+  if (!cache.has(key)) {
+    const built = buildG6U2TrapLesson(source);
+    cache.set(key, presentG6U2TrapForGrade4(built));
+  }
   return cache.get(key);
 }
