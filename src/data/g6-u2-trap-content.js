483a0db5d0f4c1a789ed09ee5738c30da4c89d49
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-08-22 11:07:48 +0700

    feat(g6-u2): attach required theory gate to trap lessons

--- src/data/g6-u2-trap-content.js
+++ src/data/g6-u2-trap-content.js
@@ -2,6 +2,7 @@ import { g6U2TrapSourceByKey } from './g6-u2-trap-source.js';
 import { buildG6U2TrapLesson } from './g6-u2-trap-builder.js';
 import { presentG6U2TrapForGrade4 } from './g6-u2-trap-grade4-presentation.js';
 import { sanitizeG6U2TrapGrade4 } from './g6-u2-trap-grade4-sanitize.js';
+import { requiredTheoryForTrap } from './g6-u2-trap-prelesson-theory.js';
 
 const cache = new Map();
 
@@ -11,7 +12,11 @@ export function getG6U2TrapContent(key) {
   if (!cache.has(key)) {
     const built = buildG6U2TrapLesson(source);
     const presented = presentG6U2TrapForGrade4(built);
-    cache.set(key, sanitizeG6U2TrapGrade4(presented));
+    const sanitized = sanitizeG6U2TrapGrade4(presented);
+    cache.set(key, Object.freeze({
+      ...sanitized,
+      preLessonTheory: requiredTheoryForTrap(source.trapCode)
+    }));
   }
   return cache.get(key);
 }
