a5082b1936852da2a428b99109cf8022014e13bc
thanhnguyendafa-ux <thanhnguyendafa@gmail.com>
2026-09-02 15:25:01 +0700

    Remove Global 6 Unit 1 Tier 2-3 vocab from published catalog

--- src/data/publishedLessonCatalog.js
+++ src/data/publishedLessonCatalog.js
@@ -16,7 +16,6 @@ import { g5U10WritingFolders, g5U10WritingRegistry } from './g5-u10-writing-typi
 import { g5ReviewU15Folders, g5ReviewU15Registry } from './g5-review-u1-5-catalog.js';
 import { g5WorkbookFolders, g5WorkbookRegistry } from './workbooks/g5/index.js';
 import { g6U1WritingFolders, g6U1WritingRegistry } from './g6-u1-writing-typing-published.js';
-import { g6U1VocabTypingFolders, g6U1VocabTypingRegistry } from './g6-u1-vocab-typing-catalog.js';
 import { g6U1WorkbookFolders, g6U1WorkbookRegistry } from './g6-u1-workbook-catalog.js';
 import { g6U2WritingFolders, g6U2WritingRegistry } from './g6-u2-writing-typing-catalog.js';
 import { g6U2WorkbookFolders, g6U2WorkbookRegistry } from './g6-u2-workbook-catalog.js';
@@ -52,10 +51,10 @@ const gradedG7WorkbookRegistry = Object.freeze([
 export const lessonFolders = Object.freeze([
   ...baseLessonFolders,...gs23WritingFolders,...g2WorkbookFolders,...g3WorkbookFolders,...g5U1WritingFolders,...g5U2WritingFolders,...g5U3WritingFolders,...g5U4WritingFolders,...g5U5WritingFolders,
   ...g5U6WritingFolders,...g5U7WritingFolders,...g5U8WritingFolders,...g5U9WritingFolders,...g5U10WritingFolders,
-  ...g5ReviewU15Folders,...g5WorkbookFolders,...g6U1WritingFolders,...g6U1VocabTypingFolders,...g6U1WorkbookFolders,...g6U2WritingFolders,...g6U2WorkbookFolders,...g6U2TrapFolders,...g6U3WorkbookFolders,...g6ReviewU13Folders,...g6WorkbookRemainingFolders,...g7U1MlhVocabContextFolders,...g6U1MlhReadingGapFolders,...g6U1MlhWritingFolders,...g7U1WritingFolders,...g7U1WorkbookFolders,...g7U2WritingFolders,...g7U2WorkbookFolders,...g7U3WorkbookFolders,...g7ReviewU13Folders
+  ...g5ReviewU15Folders,...g5WorkbookFolders,...g6U1WritingFolders,...g6U1WorkbookFolders,...g6U2WritingFolders,...g6U2WorkbookFolders,...g6U2TrapFolders,...g6U3WorkbookFolders,...g6ReviewU13Folders,...g6WorkbookRemainingFolders,...g7U1MlhVocabContextFolders,...g6U1MlhReadingGapFolders,...g6U1MlhWritingFolders,...g7U1WritingFolders,...g7U1WorkbookFolders,...g7U2WritingFolders,...g7U2WorkbookFolders,...g7U3WorkbookFolders,...g7ReviewU13Folders
 ]);
 export const lessonRegistry = Object.freeze([
   ...baseLessonRegistry,...demoMasteryEffortRegistry,...gs23WritingRegistry,...gradedG2WorkbookRegistry,...gradedG3WorkbookRegistry,...g5U1WritingRegistry,...g5U2WritingRegistry,...g5U3WritingRegistry,...g5U4WritingRegistry,...g5U5WritingRegistry,
   ...g5U6WritingRegistry,...g5U7WritingRegistry,...g5U8WritingRegistry,...g5U9WritingRegistry,...g5U10WritingRegistry,
-  ...g5ReviewU15Registry,...gradedG5WorkbookRegistry,...g6U1WritingRegistry,...g6U1VocabTypingRegistry,...g6U2WritingRegistry,...g6U2TrapRegistry,...g6ReviewU13Registry,...gradedG6WorkbookRegistry,...g7U1MlhVocabContextRegistry,...g6U1MlhReadingGapRegistry,...g6U1MlhWritingRegistry,...g7U1WritingRegistry,...g7U2WritingRegistry,...gradedG7WorkbookRegistry,...g7ReviewU13Registry
+  ...g5ReviewU15Registry,...gradedG5WorkbookRegistry,...g6U1WritingRegistry,...g6U2WritingRegistry,...g6U2TrapRegistry,...g6ReviewU13Registry,...gradedG6WorkbookRegistry,...g7U1MlhVocabContextRegistry,...g6U1MlhReadingGapRegistry,...g6U1MlhWritingRegistry,...g7U1WritingRegistry,...g7U2WritingRegistry,...gradedG7WorkbookRegistry,...g7ReviewU13Registry
 ]);
